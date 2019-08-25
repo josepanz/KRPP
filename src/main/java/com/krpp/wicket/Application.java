b722bd107769f2c0f0e7d84b096d3d4157737bd9
josepanz <josepanza1@gmail.com>
2019-08-24 22:30:11 -0400

    modificacion en el pom y en el application para heroku

--- src/main/java/com/krpp/wicket/Application.java
+++ src/main/java/com/krpp/wicket/Application.java
@@ -22,6 +22,7 @@ public class Application extends WebApplication
     @Override
     public Class<? extends WebPage> getHomePage()
     {
+        
             return LoginPage.class;
     }
     
@@ -40,5 +41,14 @@ public class Application extends WebApplication
             // add your configuration here           
 
             
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+
     }
 }
